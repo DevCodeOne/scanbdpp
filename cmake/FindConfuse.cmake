1bafb68a45a03ede1edbe1b7b88feafc09cee2b3
DevCodeOne <christian.r.dev@googlemail.com>
2017-08-28 09:32:37 +0200

    Small changes and removed license for now

--- cmake/FindConfuse.cmake
+++ cmake/FindConfuse.cmake
@@ -12,15 +12,15 @@
 #=============================================================================
 # Copyright 2010-2013, Julien Schueller
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met: 
-# 
+# modification, are permitted provided that the following conditions are met:
+#
 # 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer. 
+#    list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution. 
+#    and/or other materials provided with the distribution.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -34,7 +34,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # The views and conclusions contained in the software and documentation are those
-# of the authors and should not be interpreted as representing official policies, 
+# of the authors and should not be interpreted as representing official policies,
 # either expressed or implied, of the FreeBSD Project.
 #=============================================================================
 
@@ -66,7 +66,7 @@ include ( FindPackageHandleStandardArgs )
 find_package_handle_standard_args( Confuse DEFAULT_MSG CONFUSE_LIBRARY CONFUSE_INCLUDE_DIR  )
 
 mark_as_advanced (
-  CONFUSE_LIBRARY 
+  CONFUSE_LIBRARY
   CONFUSE_LIBRARIES
   CONFUSE_INCLUDE_DIR
   CONFUSE_INCLUDE_DIRS
