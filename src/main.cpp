f7c5048a4b9cbb9f383aa825ac4d14659cdad5b5
DevCodeOne <christian.r.dev@googlemail.com>
2017-10-04 21:09:33 +0200

    Added config structure, also updated submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,50 @@
+#include <iostream>
+
+#include "confusepp.h"
+
 int main() {
+    using namespace confusepp;
+
+    // TODO add device Multisection
+    Root config_structure{
+        Section("global")
+            .values(Option<bool>("debug"),
+                    Option<int>("debug-level"),
+                    Option<std::string>("user"),
+                    Option<std::string>("group"),
+                    Option<std::string>("saned"),
+                    Option<List<std::string>>("saned_opt"),
+                    Option<List<std::string>>("saned_env"),
+                    Option<std::string>("scriptdir"),
+                    Option<std::string>("device_insert_script"),
+                    Option<std::string>("device_remove_script"),
+                    Option<int>("timeout"),
+                    Option<std::string>("pidfile"),
+                    Section("environment")
+                        .values(Option<std::string>("device"),
+                                Option<std::string>("action")),
+                    Multisection("function")
+                        .values(Option<std::string>("filter"),
+                                Option<std::string>("desc"),
+                                Option<std::string>("env")),
+                    Option<bool>("multiple_actions"),
+                    Multisection("action")
+                        .values(Option<std::string>("filter"),
+                                Section("numerical-trigger")
+                                    .values(Option<int>("from-value"),
+                                            Option<int>("to-value")),
+                                Section("string-trigger")
+                                    .values(Option<std::string>("from-value"),
+                                            Option<std::string>("to-value")),
+                                Option<std::string>("desc"),
+                                Option<std::string>("script")))
+    };
+
+    auto conf = Config::parse_config("conf/scanbd.conf", std::move(config_structure));
+
+    if (conf) {
+        std::cout << "Config is valid" << std::endl;
+    }
+
     return 0;
 }
